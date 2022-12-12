8705c216a51acdbfd902c1db349e814c06d0628e
ChristoferMendes <christoferluizdsm@gmail.com>
2022-12-12 01:00:46 -0300

    regex for email done

--- src/commands/store-me.ts
+++ src/commands/store-me.ts
@@ -1,20 +1,25 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Command } from "gluegun/build/types/domain/command";
 import { Toolbox } from "gluegun/build/types/domain/toolbox";
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
 module.exports = {
   name: 'store-me',
   description: 'Store your git credentials (default is your local git credentials)',
   run: async (toolbox: Toolbox) => {
     const { parameters } = toolbox
-    // const emailRegex = new RegExp('/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/', 'g')
+    const emailRegex = new RegExp('^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$', 'i')
 
-    // const name = parameters.array.filter(item => !item.match(/az/))
-    const email = parameters.array.filter(item => item.match('/az/g'))
+    const name = parameters.array.filter(item => !item.match(emailRegex))[0]
+    const email = parameters.array.filter(item => item.match(emailRegex))[0]
 
-    console.log(email)
+    await prisma.user.create({
+      data: {
+        name,
+        email
+      }
+    })
 
   }
 } as Command
