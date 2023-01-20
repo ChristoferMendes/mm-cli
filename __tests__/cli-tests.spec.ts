522055730c784fe4ea7175fb655e9587e184c8d7
ChristoferMendes <christoferluizdsm@gmail.com>
2023-01-20 08:57:50 -0300

    changed the project version

--- __tests__/cli-tests.spec.ts
+++ __tests__/cli-tests.spec.ts
@@ -7,10 +7,10 @@ const cli = async (cmd) =>
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('1.0.0')
+  expect(output).toContain('1.0.9')
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('1.0.0')
+  expect(output).toContain('1.0.9')
 })
